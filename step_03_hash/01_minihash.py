22f0a1547b08b718f4361f5ab5f92e16c9e2d859
eugenii <eugenii.cher@gmail.com>
2026-03-20 20:12:59 +0300

    mini_hash take 2

--- step_03_hash/01_minihash.py
+++ step_03_hash/01_minihash.py
@@ -1,7 +1,7 @@
 # mini-hash
 
 class SimpleMap:
-    def __init__(self,size=10):
+    def __init__(self, size=10):
         """Массив корзин. Размер по умолчанию 10."""
         self.size = size
         self.table = [[] for _ in range(self.size)]
@@ -13,8 +13,11 @@ class SimpleMap:
     def put(self, key, value):
         """Добавляет пару (ключ, значение) в таблицу."""
         index = self._get_hash(key)
-        self.table[index].append((key, value))
-        print(self.table)
+        for item in self.table[index]:
+            if item[0] == key:
+                item[1] = value
+                return
+        self.table[index].append([key, value])
 
 
 # для создания теста на bash в дальнейшем...
@@ -22,9 +25,14 @@ commands = {
     'put': SimpleMap.put,
 }
 
-data = [1, 11, 21]
+# data = [1, 11, 21]
 
 table = SimpleMap()
 
-for elt in data:
-    table.put(elt, elt)
+# for elt in data:
+#     table.put(elt, elt)
+
+map = SimpleMap()
+map.put(1, "первое")
+map.put(1, "второе")
+print(map.table[1]) # Должно быть только [[1, "второе"]]
