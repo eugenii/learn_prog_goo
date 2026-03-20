b02d0bd9ac21001c4d4534538a98cff047a62568
eugenii <eugenii.cher@gmail.com>
2026-03-20 20:24:51 +0300

    mini_hash take 3

--- step_03_hash/01_minihash.py
+++ step_03_hash/01_minihash.py
@@ -17,7 +17,14 @@ class SimpleMap:
             if item[0] == key:
                 item[1] = value
                 return
-        self.table[index].append([key, value])
+        self.table[index].append([key, value])  # Изменил кортеж на список
+
+    def get(self, key):
+        index = self._get_hash(key)
+        for item in self.table[index]:
+            if item[0] == key:
+                return item[1]
+            return None
 
 
 # для создания теста на bash в дальнейшем...
@@ -32,7 +39,14 @@ table = SimpleMap()
 # for elt in data:
 #     table.put(elt, elt)
 
-map = SimpleMap()
-map.put(1, "первое")
-map.put(1, "второе")
-print(map.table[1]) # Должно быть только [[1, "второе"]]
+# take 1
+# map = SimpleMap()
+# map.put(1, "первое")
+# map.put(1, "второе")
+# print(map.table[1]) # Должно быть только [[1, "второе"]]
+
+# take 2
+my_map = SimpleMap()
+my_map.put(10, "apple")
+print(my_map.get(10))  # Должно вывести: apple
+print(my_map.get(999)) # Должно вывести: None
